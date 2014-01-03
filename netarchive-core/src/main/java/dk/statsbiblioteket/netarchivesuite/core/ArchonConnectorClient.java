25bcbaa4285290c5c554ba1b1f9cf11d0d8ff23a
Thomas Egense <thomas.egense@gmail.com>
2014-01-03 13:01:43 +0100

    Perfect handling of HTTP errors. Retrieve the original errormessage for the thrown exception. Toke gonna be so happy for this class.

--- netarchive-core/src/main/java/dk/statsbiblioteket/netarchivesuite/core/ArchonConnectorClient.java
+++ netarchive-core/src/main/java/dk/statsbiblioteket/netarchivesuite/core/ArchonConnectorClient.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.ws.rs.core.UriBuilder;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
@@ -13,7 +14,7 @@ import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class ArchonConnectorClient implements ArchonConnector{ 
 
-    private  WebResource service;
+    private  WebResource service;    
 
     /**
      * Client for the REST-based webservice provided by the netachive-archon module. 
@@ -28,63 +29,89 @@ public class ArchonConnectorClient implements ArchonConnector{
     }
 
     @Override
-    public String nextShardID(){
-        String shardID =service.path("nextShardID").get(String.class);
+    public String nextShardID(){                      
+
+        //String shardID =service.path("nextShardID").get(String.class);
+        ClientResponse response= service.path("nextShardID").get(ClientResponse.class);
+        handleHttpExceptions(response);
+        String shardID = response.getEntity(String.class);
         return shardID;
     }
 
     @Override
     public void addARC(String arcID){
         String urlencodedArcId= fixSlashUrlEncoding(arcID);       
-        service.path("addARC").path(urlencodedArcId).post();        
+        ClientResponse response = service.path("addARC").path(urlencodedArcId).post(ClientResponse.class);                    
+        handleHttpExceptions(response);                    
     }
 
     @Override
-    public String nextARC(String shardID){
-        String nextArc = service.path("nextARC").path(shardID).get(String.class);      
+    public String nextARC(String shardID){                   
+        ClientResponse response  = service.path("nextARC").path(shardID).get(ClientResponse.class);
+        handleHttpExceptions(response);
+        String nextArc = response.getEntity(String.class);
         return nextArc;
     }
 
     @Override
     public void setARCState(String arcID, ARC_STATE state){
-        String urlencodedArcId= fixSlashUrlEncoding(arcID); 
-        service.path("setARCState").path(urlencodedArcId).path(state.toString()).post();        
+        String urlencodedArcId= fixSlashUrlEncoding(arcID);         
+        ClientResponse response = service.path("setARCState").path(urlencodedArcId).path(state.toString()).post(ClientResponse.class);
+        handleHttpExceptions(response);
     }
 
     @Override
     public void clearIndexing(String shardID){
-        service.path("clearIndexing").path(shardID).post();   
+        //  service.path("clearIndexing").path(shardID).post();   
+        ClientResponse response  = service.path("clearIndexing").path(shardID).post(ClientResponse.class);
+        handleHttpExceptions(response);        
     }
 
     @Override
     public void removeARC(String arcID){
         String urlencodedArcId= fixSlashUrlEncoding(arcID); 
-        service.path("removeARC").path(urlencodedArcId).post();              
+        ClientResponse response = service.path("removeARC").path(urlencodedArcId).post(ClientResponse.class);
+        handleHttpExceptions(response);
     }
 
     @Override
     public List<String> getShardIDs(){     
-        StringListWrapper wrapper = service.path("getShardIDs").get(StringListWrapper.class);
+        ClientResponse response =  service.path("getShardIDs").get(ClientResponse.class);
+        handleHttpExceptions(response);
+        StringListWrapper wrapper = response.getEntity(StringListWrapper.class);
         ArrayList<String> values = wrapper.getValues();          
         return values;              
     }
 
     @Override
     public List<String> getARCFiles(String shardID){
-        StringListWrapper wrapper = service.path("getARCFiles").path(shardID).get(StringListWrapper.class);
+        ClientResponse response = service.path("getARCFiles").path(shardID).get(ClientResponse.class);
+        handleHttpExceptions(response);        
+        StringListWrapper wrapper = response.getEntity(StringListWrapper.class);
         ArrayList<String> values = wrapper.getValues();        
         return values;     
     }
 
     @Override 
     public void setARCProperties(String arcID, String shardID, ARC_STATE state, int priority){              
-        String urlencodedArcId= fixSlashUrlEncoding(arcID);
-        service.path("setARCProperties").path(urlencodedArcId).path(shardID).path(state.toString()).path(""+priority).post();  
+        String urlencodedArcId= fixSlashUrlEncoding(arcID);     
+        ClientResponse response = service.path("setARCProperties").path(urlencodedArcId).path(shardID).path(state.toString()).path(""+priority).post(ClientResponse.class);  
+        handleHttpExceptions(response);
     }
 
     @Override
     public void setShardState(String shardID, ARC_STATE state, int priority){       
-        service.path("setShardState").path(shardID).path(state.toString()).path(""+priority).post();       
+        ClientResponse response =  service.path("setShardState").path(shardID).path(state.toString()).path(""+priority).post(ClientResponse.class);       
+        handleHttpExceptions(response);
+    }
+
+    private void handleHttpExceptions(ClientResponse response) {
+        //200 is OK also 204 is OK (no content)
+        if (!(response.getStatus() == 200 || response.getStatus() == 204)) {
+            int status = response.getStatus();
+            String content_message = response.getEntity(String.class);                        
+            throw new IllegalArgumentException("HTTP code:"+status +" error:"+content_message);                     
+        }
     }
 
     /*
