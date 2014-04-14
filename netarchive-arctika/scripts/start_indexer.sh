35a75fdf82451159528d4743291aa5ff503d64cd
Thomas Egense <thomas.egense@gmail.com>
2014-04-14 11:14:06 +0200

    start_indexer script

---
+++ netarchive-arctika/scripts/start_indexer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -Xmx10g -DArtikaPropertyFile=/home/summanet/arctika/arctika.properties -Dlog4j.configuration=file:/home/summanet/arctika/log4j.properties -jar netarchive-arctika-1.0.jar </dev/null >>nohup.out 2>&1 &
+                                                                                                                                                                                                                                
+
