4db5db8647a9ed219b65299c7a5a7681f6cba0ef
Thomas Egense <thomas.egense@gmail.com>
2014-04-16 08:13:08 +0200

    scripts

--- netarchive-arctika/scripts/register_add_ALL_arc_files.sh
+++ netarchive-arctika/scripts/register_add_ALL_arc_files.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-for d in $(ls -d /netarkiv/*/*/filedir /netarkiv/*/filedir 2>/dev/null)
+for ARC_DIR in $(ls -d /netarkiv/*/*/filedir /netarkiv/*/filedir 2>/dev/null)
 do
-  echo "processing: $f"
-  ## urlencode all / to %2F
-  f_urlencoded=`echo $f | sed 's/\//%2F/g'`
-  ##echo "curl --data '' http://belinda:9721/netarchive-archon/services/addARC/$f_urlencoded"  
-
-  curl --data ' ' http://belinda:9721/netarchive-archon/services/addARC/$f_urlencoded
-
+  echo "processing directory: $ARC_DIR"
+  for f in $(find $ARC_DIR -maxdepth 1 -type f -name '*.arc' 2>/dev/null)
+  do
+    full_path=$f
+    echo "processing arc-file: $full_path"
+    ## urlencode all / to %2F
+    f_urlencoded=${full_path////%2F}
+    ##echo "curl --data '' http://belinda:9721/netarchive-archon/services/addARC/$f_urlencoded"  
+    curl --data ' ' http://belinda:9721/netarchive-archon/services/addARC/$f_urlencoded 
+   done
 done
+
+# vim: set ft=sh sw=2 sts=2 : #
