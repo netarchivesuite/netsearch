d9bbcdb7d4504e9743eec72667e6f9fbb965b345
Mads Villadsen <mv@statsbiblioteket.dk>
2014-10-15 10:26:10 +0200

    Added waybackURL with substitution from current document for keys in {}

--- netarchive-blacklight/search_app/app/helpers/blacklight_helper.rb
+++ netarchive-blacklight/search_app/app/helpers/blacklight_helper.rb
@@ -4,12 +4,23 @@ module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
 
   def link_to_document (doc, opts={:label=>nil, :counter => nil})
-    waybackBaseURL = 'http://localhost:8080/showmebyid'
+    waybackURL = 'http://elara.statsbiblioteket.dk/wayback/{wayback_date}/{url}'
+
+    # run through all {} keys in the configured URL and replace them
+    # with values from the current document 
+    actualURL = waybackURL;
+    expando = waybackURL.scan(/\{\w+\}/)
+    expando.each {
+        |exp|
+        key = exp.gsub(/[{}]/, '')
+        actualURL = actualURL.gsub('{'+key+'}', doc[key])
+    }
+
     title = 'No title found'
     if doc['title'] != nil
         title = doc['title'][0]
     end
 
-    link_to title, waybackBaseURL + '?id=' + CGI::escape(doc['id'])
+    link_to title, actualURL
   end
 end
