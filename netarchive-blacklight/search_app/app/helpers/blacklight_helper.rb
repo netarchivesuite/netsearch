d51e7e558db3fcf341fad491ca2c43a7ca990a43
Mads Villadsen <mv@statsbiblioteket.dk>
2014-10-15 11:11:18 +0200

    Add support for url encoded expansion in wayback url. Move wayback url configuration to catalog_controller with the rest of the configuration

--- netarchive-blacklight/search_app/app/helpers/blacklight_helper.rb
+++ netarchive-blacklight/search_app/app/helpers/blacklight_helper.rb
@@ -4,17 +4,30 @@ module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
 
   def link_to_document (doc, opts={:label=>nil, :counter => nil})
-    waybackURL = 'http://elara.statsbiblioteket.dk/wayback/{wayback_date}/{url}'
+    waybackURL = blacklight_config.wayback_url || ''
 
-    # run through all {} keys in the configured URL and replace them
+    # run through all keys in the configured URL and replace them
     # with values from the current document 
     actualURL = waybackURL;
     expando = waybackURL.scan(/\{\w+\}/)
-    expando.each {
+    expandoURLEncode = waybackURL.scan(/\[\w+\]/)
+
+    # replace ordinary keys
+    expando.each do
         |exp|
         key = exp.gsub(/[{}]/, '')
-        actualURL = actualURL.gsub('{'+key+'}', doc[key])
-    }
+        value = doc[key]
+        actualURL = actualURL.gsub('{'+key+'}', value)
+    end
+
+    # replace keys that need to be url encoded
+    expandoURLEncode.each do
+        |exp|
+        key = exp.gsub(/[\[\]]/, '')
+        value = doc[key]
+        value = CGI::escape(value)
+        actualURL = actualURL.gsub('['+key+']', value)
+    end
 
     title = 'No title found'
     if doc['title'] != nil
