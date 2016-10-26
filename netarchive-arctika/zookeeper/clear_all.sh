36142cec8b68454efd4a81406a4a6c2a580161be
Thomas Egense <thomas.egense@gmail.com>
2016-10-26 10:00:34 +0200

    release folder removed.

---
+++ netarchive-arctika/zookeeper/clear_all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -r zookeeper1/zoo_data/version-2/
+rm -r zookeeper2/zoo_data/version-2/
+rm -r zookeeper3/zoo_data/version-2/
+rm -r zookeeper3/zoo_datalog/version-2/
+rm -r zookeeper2/zoo_datalog/version-2/
+rm -r zookeeper1/zoo_datalog/version-2/
+
---
+++ netarchive-arctika/zookeeper/stop_all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+zookeeper1/bin/zkServer.sh stop
+zookeeper2/bin/zkServer.sh stop
+zookeeper3/bin/zkServer.sh stop
+
