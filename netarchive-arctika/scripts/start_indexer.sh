0cc602b93f8127819cbd3c18e16a551114659e4e
Thomas Egense <thomas.egense@gmail.com>
2016-10-26 09:59:25 +0200

    release folder removed.

--- netarchive-arctika/scripts/start_indexer.sh
+++ netarchive-arctika/scripts/start_indexer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -Xmx10g -DArtikaPropertyFile=/home/summanet/arctika/arctika.properties -Dlog4j.configuration=file:/home/summanet/arctika/log4j.properties -jar netarchive-arctika-1.0.jar </dev/null >>nohup.out 2>&1 &
+java -Xmx10g -DArtikaPropertyFile=/home/summanet/arctika/arctika.properties -Dlog4j.configuration=file:/home/summanet/arctika/log4j.properties -jar netarchive-arctika-1.2.jar </dev/null >>nohup.out 2>&1 &
                                                                                                                                                                                                                                 
 
