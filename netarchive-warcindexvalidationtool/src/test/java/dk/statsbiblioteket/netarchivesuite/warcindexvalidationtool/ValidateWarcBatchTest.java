79f39ff2fb2996698dd7a0ad1b0432eff7ccce05
Thomas Egense <thomas.egense@gmail.com>
2017-09-13 14:33:08 +0200

    Test method to valdiate multiple warc files at once

---
+++ netarchive-warcindexvalidationtool/src/test/java/dk/statsbiblioteket/netarchivesuite/warcindexvalidationtool/ValidateWarcBatchTest.java
@@ -0,0 +1,39 @@
+package dk.statsbiblioteket.netarchivesuite.warcindexvalidationtool;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+
+public class ValidateWarcBatchTest {
+
+  
+  /*
+   * This is just a batched version of the validate warc method.
+   * The input is a file with 1 arc file pr. line
+   * 
+   */
+  
+  public static void main(String[] args) throws Exception{
+    
+      
+    //String solr = "http://ariel:52300/solr/collection1"; //Leave as null if you dont want to validate against a solr index
+    String solr = "http://localhost:8983/solr/netarchivebuilder/";
+    
+          
+    //Will just validate all filenames in this text-file. (1 warcfile pr. line)
+    String warcFileList= "warcs.txt"; //in resources folder
+    Paths.get(warcFileList);
+  
+    HashSet<Integer> httpStatusPrefix = new HashSet<Integer>();
+    httpStatusPrefix.add(2); //2XX
+    httpStatusPrefix.add(404); //2XX
+    List<String> lines = Files.readAllLines(Paths.get(warcFileList));
+
+    for (String line : lines){      
+      ValidateWarc validateWarc = new ValidateWarc(line,solr,httpStatusPrefix);
+      validateWarc.validate();      
+    }
+  }
+   
+}
